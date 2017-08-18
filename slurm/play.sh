d39f4b52432fcc51630e9b2d45fdc9b70098e606
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-08-18 17:39:58 +0200

    using playbooks folder, added ansible.cfg

--- slurm/play.sh
+++ slurm/play.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#ansible-playbook -c local -i hosts.example --become --ask-become-pass site.yml
-ansible-playbook -c local -i hosts.example site.yml
+ansible-playbook -c local playbooks/slurm.yml
