34ddf0c14c16e91057bfc8790678fdb9584f1660
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-08-24 10:54:39 +0200

    refactored bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,70 @@
 #!/bin/bash
-echo "Bootstrapping ..."
 
-apt-get update -y && apt-get install -y curl bzip2
+usage() {
+  cat <<EOT
+  Usage : bootstrap.sh [option]
 
-cd /root
-curl -LO https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
-bash Miniconda2-latest-Linux-x86_64.sh -b -p /opt/conda
+  Options:
+    -h   - Print this help message.
+    -i   - Install system packages.
+EOT
+  exit 1
+}
 
-export PATH=/opt/conda/bin:$PATH
+install_pkgs() {
+  if [ -f /etc/debian_version ] ; then
+    echo "Install Debian/Ubuntu packages for Ansible ..."
+    apt-get update --fix-missing
+    apt-get install -y curl bzip2 ca-certificates \
+      libglib2.0-0 libxext6 libsm6 libxrender1
+  elif [ -f /etc/redhat-release ] ; then
+    echo "Install CentOS packages for Ansible ..."
+    yum update -y && yum install -y curl bzip2
+  elif [ `uname -s` = "Darwin" ] ; then
+    echo "Install Homebrew packages for Ansible ..."
+    brew install curl libmagic
+  fi
+}
 
-conda install -y -c conda-forge ansible
+install_conda() {
+  if [ ! -d /opt/conda ] ; then
+    curl -L -o /tmp/miniconda.sh https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
+    bash /tmp/miniconda.sh -b -p /opt/conda
+    rm /tmp/miniconda.sh
+  fi
+  export PATH=/opt/conda/bin:$PATH
+}
 
-echo "Bootstrapping done"
+install_ansible() {
+  conda install -y -c conda-forge ansible
+}
+
+bootstrap() {
+  echo "Bootstrapping ..."
+  if [ $# -gt 0 ] && [ $1 = '-i' ]; then
+    install_pkgs
+  fi
+  if [ $# -eq 0 ] || [ $1 = '-b' ] || [ $1 = '-i' ]; then
+    install_conda
+    install_ansible
+  fi
+  echo "Bootstrapping done"
+}
+
+# Handling arguments
+
+if [ $# -gt 1 ]; then
+  echo -e "Too many arguments.\n"
+  usage
+fi
+
+if [ $# -gt 0 ] && [ $1 = '-h' ]; then
+  usage
+fi
+
+if [ $# -eq 0 ] || [ $1 = '-b' ] || [ $1 = '-i' ]; then
+  bootstrap $@
+else
+  echo -e "Unknown option: $1.\n"
+  usage
+fi
