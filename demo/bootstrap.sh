d4ff8f9f0d813bca3cf543c7fca2c671c503f11c
Carsten Ehbrecht <ehbrecht@dkrz.de>
2018-01-11 18:07:11 +0100

    update bootstrap

--- demo/bootstrap.sh
+++ demo/bootstrap.sh
@@ -25,7 +25,7 @@ install_pkgs() {
         sudo apt-get -y update
         sudo apt-get -y install software-properties-common
         # install base packages
-        sudo apt-get -y install build-essential git curl
+        sudo apt-get -y install build-essential
         # install ansible
         sudo apt-add-repository -y ppa:ansible/ansible
         sudo apt-get -y update
