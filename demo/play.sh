84ab52b0688af1a2443e6778920537d98f88e401
Pingu Carsti <ehbrecht@dkrz.de>
2018-05-16 18:32:19 +0200

    removed play.sh in favour of Makefile

--- demo/play.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "\nInstalling required Ansible roles from ansible-galaxy ..."
-ansible-galaxy -p roles -r requirements.yml install
-
-echo "\nInstalling application with Ansible ..."
-ansible-playbook -c local playbook.yml
